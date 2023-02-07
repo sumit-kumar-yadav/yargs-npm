6a38a18d3fe909668269ab1f7df290117ccd2748
Sumit Yadav <sumityadav0376@gmail.com>
2023-02-08 02:49:57 +0530

    Debug the Node.js app

--- notes.js
+++ notes.js
@@ -1,9 +1,26 @@
 const fs = require('fs')
 const chalk = require('chalk')
+const { command } = require('yargs')
+const { type } = require('os')
 
 const addNote = (title, body) => {
     const notes = loadNotes()
     const duplicateNote = notes.find((note) => note.title === title)
+
+    debugger;
+    // Adding debugger here to stop the program at this particular point
+    // while running the Node.js app with command 
+    // node inspect app.js add --title="Courses" --body="Node.js"
+
+    // Note that we are adding inspect in the origial command for debugging 
+
+    // Now after running the command in the terminal open chrome and type in the url
+    // chrome://inspect
+
+    // After clicking on inspect in remote target, click on Filesystem and add the whole current Node.js project in it 
+    // You need to allow the permission 
+    // After that wherever the debugger is PerformanceObserverEntryList, your program will stop and you can inspect. 
+    // We can restart the debug with command --> restart
 
     if (!duplicateNote) {
         notes.push({
